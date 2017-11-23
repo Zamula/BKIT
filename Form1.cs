ae7521d83ce3fef1126bcf914ab7601f2672d096
Zamula <33939281+Zamula@users.noreply.github.com>
2017-11-23 21:56:19 +0300

    лаб4

---
+++ Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WindowsFormsFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// Список слов
+
+        List<string> list = new List<string>();
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+            
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                //Чтение файла в виде строки
+                string text = File.ReadAllText(fd.FileName);
+
+                //Разделительные символы для чтения из файла
+                char[] separators = new char[] {' ','.',',','!','?','/','\t','\n'};
+
+                string[] textArray = text.Split(separators);
+
+                foreach (string strTemp in textArray)
+                {
+                    //Удаление пробелов в начале и конце строки
+                    string str = strTemp.Trim();
+
+                    int check = 0;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (EditDistance.Distance(str, list[i]) != 0) check += 1;
+                        else
+                            break;
+                    }
+                    if (check == list.Count) list.Add(str);
+
+                    //Добавление строки в список, если строка не содержится в списке
+                    //if (!list.Contains(str)) list.Add(str);
+
+
+
+
+                }
+
+                t.Stop();
+                //время
+                this.textBoxFileReadTime.Text = t.Elapsed.ToString();
+                this.textBoxFileReadCount.Text = list.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл");
+            }
+        }
+
+
+
+
+
+        private void buttonExact_Click(object sender, EventArgs e)
+        {
+            //Слово для поиска
+            string word = this.textBoxFind.Text.Trim();
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                //Слово для поиска в верхнем регистре
+                string wordUpper = word.ToUpper();
+
+                //Временные результаты поиска
+                List<string> tempList = new List<string>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                foreach (string str in list)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+
+                word =word + '-' + t.Elapsed.ToString() + '\n';
+                this.listBoxResult.BeginUpdate();
+
+                //Вывод результатов поиска 
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+
+
+                this.listBoxResult.Items.Add(word);
+
+                
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+
+        }
+
+
+
+
+        private void buttonApprox_Click(object sender, EventArgs e)
+        {
+            //Слово для поиска
+            string word = this.textBoxFind.Text.Trim();
+
+            //Если слово для поиска не пусто
+            if (!string.IsNullOrWhiteSpace(word) && list.Count > 0)
+            {
+                
+
+                
+
+                //начало поиска
+
+
+                //Результирующий список  
+                List<ParallelSearchResult> Result = new List<ParallelSearchResult>();
+
+              
+
+              
+               
+
+                //Вывод результатов
+
+               
+
+                //Начало обновления списка результатов
+                this.listBoxResult.BeginUpdate();
+
+                //Очистка списка
+                this.listBoxResult.Items.Clear();
+
+                //Вывод результатов поиска 
+                foreach (var x in Result)
+                {
+                    string temp = x.word + "(расстояние=" + x.dist.ToString() + " поток=" + x.ThreadNum.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }
+
+                //Окончание обновления списка результатов
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+
+        }
+
+        /// <summary>
+        /// Выполняется в параллельном потоке для поиска строк
+        /// </summary>
+        public static List<ParallelSearchResult> ArrayThreadTask(object paramObj)
+        {
+            ParallelSearchThreadParam param = (ParallelSearchThreadParam)paramObj;
+
+            //Слово для поиска в верхнем регистре
+            string wordUpper = param.wordPattern.Trim().ToUpper();
+
+            //Результаты поиска в одном потоке
+            List<ParallelSearchResult> Result = new List<ParallelSearchResult>();
+
+            //Перебор всех слов во временном списке данного потока 
+            foreach (string str in param.tempList)
+            {
+                //Вычисление расстояния Дамерау-Левенштейна
+                int dist = EditDistance.Distance(str.ToUpper(), wordUpper);
+
+                //Если расстояние меньше порогового, то слово добавляется в результат
+                if (dist <= param.maxDist)
+                {
+                    ParallelSearchResult temp = new ParallelSearchResult()
+                    {
+                        word = str,
+                        dist = dist,
+                        ThreadNum = param.ThreadNum
+                    };
+
+                    Result.Add(temp);
+                }
+            }
+            return Result;
+        }
+
+
+
+
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+
+        private void buttonSaveReport_Click(object sender, EventArgs e)
+        {
+            //Имя файла отчета
+            string TempReportFileName = "Report_" + DateTime.Now.ToString("dd_MM_yyyy_hhmmss");
+
+            //Диалог сохранения файла отчета
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.FileName = TempReportFileName;
+            fd.DefaultExt = ".html";
+            fd.Filter = "HTML Reports|*.html";
+
+
+
+
+
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string ReportFileName = fd.FileName;
+
+                //Формирование отчета
+                StringBuilder b = new StringBuilder();
+                b.AppendLine("<html>");
+                
+                b.AppendLine("<head>");
+                b.AppendLine("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
+                b.AppendLine("<title>" + "Отчет: " + ReportFileName + "</title>");
+                b.AppendLine("</head>");
+
+                b.AppendLine("<body>");
+
+                b.AppendLine("<h1>" + "Отчет: " + ReportFileName + "</h1>");
+                b.AppendLine("<table border='1'>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время чтения из файла</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadTime.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Количество уникальных слов в файле</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadCount.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Слово для поиска</td>");
+                b.AppendLine("<td>" + this.textBoxFind.Text + "</td>");
+                b.AppendLine("</tr>");
+
+                
+
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время четкого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxExactTime.Text + "</td>");
+                b.AppendLine("</tr>");
+
+               
+                b.AppendLine("<tr valign='top'>");
+                b.AppendLine("<td>Результаты поиска</td>");
+                b.AppendLine("<td>");               
+                b.AppendLine("<ul>");
+
+                foreach (var x in this.listBoxResult.Items)
+                {
+                    b.AppendLine("<li>" + x.ToString() + "</li>");
+                }
+
+                b.AppendLine("</ul>");
+                b.AppendLine("</td>");
+                b.AppendLine("</tr>");
+
+                b.AppendLine("</table>");
+
+                b.AppendLine("</body>");               
+                b.AppendLine("</html>");
+
+                //Сохранение файла
+                File.AppendAllText(ReportFileName, b.ToString());
+
+                MessageBox.Show("Отчет сформирован. Файл: " + ReportFileName);
+            }
+        }
+
+    }
+}
